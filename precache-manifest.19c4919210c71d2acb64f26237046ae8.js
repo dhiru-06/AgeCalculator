b0b6ce6165cccc69848a58b42bb6b9855f3e346c
dhiru-06 <dhiru199706@gmail.com>
2020-06-23 19:46:47 +0530

    Updates

---
+++ precache-manifest.19c4919210c71d2acb64f26237046ae8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223de1295119a8dbd5131b82f76549a4",
+    "url": "/AgeCalculator/index.html"
+  },
+  {
+    "revision": "b6a1c1847d031e82fbf2",
+    "url": "/AgeCalculator/static/css/main.48833752.chunk.css"
+  },
+  {
+    "revision": "72ae4c8f17a665a96ffd",
+    "url": "/AgeCalculator/static/js/2.25ee997e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/AgeCalculator/static/js/2.25ee997e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6a1c1847d031e82fbf2",
+    "url": "/AgeCalculator/static/js/main.2ddf572f.chunk.js"
+  },
+  {
+    "revision": "b34b3e39b2a48d767baa",
+    "url": "/AgeCalculator/static/js/runtime-main.df3ced9d.js"
+  }
+]);
